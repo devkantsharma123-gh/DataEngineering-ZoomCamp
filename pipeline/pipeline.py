b24c6f20cc23d186bf7b38d989ba315236fefc09
devkantsharma123-gh <devkantsharma123@gmail.com>
2026-06-01 09:53:35 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+
+print("arguments", sys.argv)
+
+month = int(sys.argv[1])
+print(f"Running pipeline for month {month}")
+
+df = pd.DataFrame({"day": [1, 2], "num_paasangers": [3, 4]})
+df['month']= month
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet")
+
+#df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
